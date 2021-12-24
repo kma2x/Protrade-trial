186515b726d085db592f496caf9ed879a852b699
kma2x <50726229+kma2x@users.noreply.github.com>
2021-12-24 09:10:38 +0700

    fix volume-percent

--- js/index.js
+++ js/index.js
@@ -1,15 +1,6 @@
-var arr_volumeBuy=[];
-var arr_volumeSell=[];
-var i=0;
-var j=0;
-var volumeBuy = document.getElementsByClassName("volume-buy")
-for (key of volumeBuy) {
-    arr_volumeBuy[i]=parseInt(key.innerText);
+var arr=[];
+var volumeBuy = document.getElementsByClassName("volume")
+for (key of volume) {
+    arr[i]=parseInt(key.innerText);
     i++;
-}
-
-var volumeSell = document.getElementsByClassName("volume-sell")
-for (key of volumeSell) {
-    arr_volumeSell[j]=parseInt(key.innerText);
-    j++;
 }
